5c5bc1a0139f12d4909cc83408e73d91e4e39cbc
Dylan Lowrie <dylan_jal@protonmail.com>
2024-05-29 14:39:35 +0100

    RPS-Test (Cypress)

--- CodeSpace - Software Development/Testing Technologies and Programming EC149760/Cypress/RPS-Test/cypress/e2e/rps.cy.js
+++ CodeSpace - Software Development/Testing Technologies and Programming EC149760/Cypress/RPS-Test/cypress/e2e/rps.cy.js
@@ -2,21 +2,78 @@ describe('RPS Game', () => {
 
   // open URL (before each test)
   beforeEach(() => {
-    cy.visit('file:///C:/Users/dylan/EC-CodeSpace-/EC-CodeSpace-/CodeSpace%20-%20Software%20Development/Testing%20Technologies%20and%20Programming%20EC149760/Cypress/RPS-Test/Rock%2C%20Paper%2C%20Scissors.html')
+    cy.visit('http://127.0.0.1:5500/RPS.html')
+    cy.get('[data-cy="rock"]').as('rock')
+    cy.get('[data-cy="paper"]').as('paper')
+    cy.get('[data-cy="scissors"]').as('scissors')
+
+    cy.get('[data-cy="user-option"]').as('userOption')
+    cy.get('[data-cy="computer-option"]').as('computerOption')
+    cy.get('[data-cy="result"]').as('result')
   })
 
 
   // Test 'Rock'
   it('tests Rock option', () => {
-    
+
+    cy.get('@rock').should('exist').click()
+
+    cy.get('@userOption').should('have.text', 'Rock')
+
+    cy.get('@computerOption').then((option) => {
+      if(option.text().includes("Rock")) {
+        cy.get('@result').contains("It is a tie!")
+      }
+      else if (option.text().includes("Paper")) {
+        cy.get('@result').contains("You lose!")
+      }
+      else {
+       cy.get('@result').contains("You win!")
+      }
+
+    })
   })
+
 
   // Test 'Paper' 
   it('tests Paper option', () => {
-    
+
+    cy.get('@paper').should('exist').click()
+
+    cy.get('@userOption').should('have.text', 'Paper')
+
+    cy.get('@computerOption').then((option) => {
+      if(option.text().includes("Paper")) {
+        cy.get('@result').contains("It is a tie!")
+      }
+
+      else if (option.text().includes("Scissors")) {
+        cy.get('@result').contains("You lose!")
+      }
+
+      else {
+        cy.get('@result').contains("You win!")
+      }
+    })
   })  
   // Test 'Scissors'
   it('tests Scissors option', () => {
-    
+    cy.get('@scissors').should('exist').click()
+
+    cy.get('@userOption').should('have.text', 'Scissors')
+
+    cy.get('@computerOption').then((option) => {
+      if(option.text().includes("Scissors")) {
+        cy.get('@result').contains("It is a tie!")
+      }
+
+      else if (option.text().includes("Rock")) {
+        cy.get('@result').contains("You lose!")
+      }
+
+      else {
+        cy.get('@result').contains("You win!")
+      }
+  })
   })
 })
