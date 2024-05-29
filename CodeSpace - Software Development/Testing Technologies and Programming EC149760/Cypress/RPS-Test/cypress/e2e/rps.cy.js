83d27a0d9b1402cf8805eac4d099446f23dbbeae
Dylan Lowrie <dylan_jal@protonmail.com>
2024-05-29 12:49:50 +0100

    EC-CodeSpace-

---
+++ CodeSpace - Software Development/Testing Technologies and Programming EC149760/Cypress/RPS-Test/cypress/e2e/rps.cy.js
@@ -0,0 +1,22 @@
+describe('RPS Game', () => {
+
+  // open URL (before each test)
+  beforeEach(() => {
+    cy.visit('file:///C:/Users/dylan/EC-CodeSpace-/EC-CodeSpace-/CodeSpace%20-%20Software%20Development/Testing%20Technologies%20and%20Programming%20EC149760/Cypress/RPS-Test/Rock%2C%20Paper%2C%20Scissors.html')
+  })
+
+
+  // Test 'Rock'
+  it('tests Rock option', () => {
+    
+  })
+
+  // Test 'Paper' 
+  it('tests Paper option', () => {
+    
+  })  
+  // Test 'Scissors'
+  it('tests Scissors option', () => {
+    
+  })
+})
